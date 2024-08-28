77e3aba99d585d2b37045c996b0911bd3eb81a5a
tristan <tristanbidault@gmail.com>
2024-08-28 14:40:56 +0200

    add homepage and form

---
+++ cvtheque/src/client/src/candidaturePlugin/components/formBlock6.vue
@@ -0,0 +1,222 @@
+import { inject } from 'vue';
+<template>
+  <div class="formBlockLayout">
+    <form class="formBlock1" @submit.prevent="onSubmit">
+      <h3 class="formTitle">Pour finir</h3>
+      <input
+        id="name"
+        v-model="name"
+        class="blackPlaceholder"
+        placeholder="Nom"
+      />
+      <input
+        id="surname"
+        v-model="surname"
+        class="blackPlaceholder"
+        placeholder="Prénom"
+      />
+      <input
+        id="email"
+        v-model="email"
+        class="blackPlaceholder"
+        placeholder="E-mail"
+      />
+      <input
+        id="phone"
+        v-model="phone"
+        class="blackPlaceholder"
+        placeholder="Téléphone"
+      />
+      <div class="AcceptanceofTerms-container">
+        <div class="checkbox-container">
+          <label class="switch">
+            <input v-model="createAccount" type="checkbox" @change="toggle" />
+            <span class="slider round"></span>
+          </label>
+          <label class="AcceptanceofTermsText">
+            Je souhaite créer un compte pour pouvoir modifier mes informations
+            ultérieurement ou mettre en pause ma recherche d’emploi.
+          </label>
+        </div>
+        <div v-if="createAccount" class="passwordFields">
+          <input
+            id="password"
+            v-model="password"
+            type="password"
+            class="blackPlaceholder"
+            placeholder="Mot de passe"
+            required
+          />
+          <input
+            id="confirmPassword"
+            v-model="confirmPassword"
+            type="password"
+            class="blackPlaceholder"
+            placeholder="Confirmer le mot de passe"
+            required
+          />
+        </div>
+        <div class="checkbox-container">
+          <label class="switch short-switch">
+            <input type="checkbox" @change="toggle" />
+            <span class="slider round"></span>
+          </label>
+          <label class="AcceptanceofTermsText">
+            J’accepte d’être contacté par des entreprises qui recrutent et qui
+            sont intéressées par mon profil.
+          </label>
+        </div>
+        <div class="checkbox-container">
+          <label class="switch short-switch">
+            <input v-model="checked" type="checkbox" @change="toggle" />
+            <span class="slider round"></span>
+          </label>
+          <label class="AcceptanceofTermsText">
+            J’ai lu et j’accepte les conditions générales ainsi que la politique
+            de confidentialité des données.
+          </label>
+        </div>
+      </div>
+      <input class="submitButton" type="submit" value="ENVOYER" />
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormSix',
+  emits: ['situation-submitted'],
+  data() {
+    return {
+      name: '',
+      surname: '',
+      email: '',
+      phone: '',
+      createAccount: false,
+      password: '',
+      confirmPassword: '',
+      checked: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (
+        this.name === '' ||
+        this.surname === '' ||
+        this.phone === '' ||
+        this.email === ''
+      ) {
+        alert("L'évaluation est incomplète. Veuillez remplir tous les champs.");
+        return;
+      }
+      if (this.createAccount && this.password !== this.confirmPassword) {
+        alert('Les mots de passe ne correspondent pas. Veuillez réessayer.');
+        return;
+      }
+      if (!this.checked) {
+        alert(
+          'Vous devez accepter les conditions générales et la politique de confidentialité des données.',
+        );
+        return;
+      }
+      let productReview = {
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        phone: this.phone,
+      };
+      if (this.createAccount) {
+        productReview.password = this.password;
+        productReview.confirmPassword = this.confirmPassword;
+      }
+      this.$emit('situation-submitted', productReview);
+      this.name = '';
+      this.surname = '';
+      this.email = '';
+      this.phone = '';
+    },
+  },
+};
+</script>
+
+<style src="./form-component.scss" lang="scss"></style>
+<style scoped>
+.AcceptanceofTermsText {
+  padding-left: 1.5rem;
+  font-family: 'Telegraf', sans-serif;
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 46px;
+  height: 14px;
+}
+.short-switch {
+  width: 34.22px;
+}
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 18px;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 12.5px;
+  width: 12.5px;
+  left: 1px;
+  bottom: 0.7px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: #31ea3d;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #31ea3d;
+}
+
+input:checked + .slider:before {
+  /* -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px); */
+  transform: translateX(13px);
+}
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+.passwordFields > * {
+  flex: 1;
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+  padding-top: 0.65rem;
+  padding-bottom: 0.65rem;
+}
+.passwordFields input::placeholder {
+  text-indent: -0.75rem;
+}
+.passwordFields input {
+  width: 100%;
+  box-sizing: border-box;
+  padding-left: 1rem;
+}
+</style>
---
+++ cvtheque/src/client/src/candidaturePlugin/components/inputFile.vue
@@ -0,0 +1,90 @@
+<template>
+  <label class="file-select">
+    <div class="select-button">
+      <div>
+        <span v-if="value">Selected File: {{ value.name }}</span>
+        <span v-else>Télecharger</span>
+      </div>
+      <div class="download-icon-wrapper">
+        <svg
+          width="21"
+          height="18"
+          viewBox="0 0 21 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 9V14.3333C1 16.3333 1.54902 17 3.19608 17H17.4706C19.1176 17 19.6667 16.3333 19.6667 14.3333V9"
+            stroke="#071E22"
+            stroke-width="1.5"
+            stroke-linecap="round"
+          />
+          <path
+            d="M10.0664 11.6667V1M10.0664 1L6.06641 5M10.0664 1L14.0664 5"
+            stroke="#071E22"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+    </div>
+    <input type="file" @change="handleFileChange" />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    value: File,
+  },
+  emits: ['emit-input'],
+  methods: {
+    handleFileChange(e) {
+      this.$emit('emit-input', e.target.files[0]);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.download-icon {
+  max-width: 1.15rem;
+  height: auto;
+  background-color: rgba(255, 255, 255, 1);
+}
+
+.select-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  color: black;
+  text-align: center;
+  font-weight: 400;
+  background-color: rgba(255, 255, 255, 1);
+  transition: background-color 0.3s;
+}
+
+.file-select {
+  background-color: rgba(255, 255, 255, 1);
+  margin-top: 0.25rem;
+  border-color: black;
+  border-width: 1px;
+  border-style: solid;
+}
+
+.download-icon-wrapper {
+  display: inline-block;
+  padding: 0.2rem;
+}
+
+.file-select > .select-button:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.file-select input[type='file'] {
+  display: none;
+}
+</style>
