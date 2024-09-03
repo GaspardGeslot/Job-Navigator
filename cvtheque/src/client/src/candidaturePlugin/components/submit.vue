550cefaf1083aa50de544ac405c840d22ae74321
tristan <tristanbidault@gmail.com>
2024-09-03 15:16:29 +0200

    fix media screen for submit component

--- cvtheque/src/client/src/candidaturePlugin/components/submit.vue
+++ cvtheque/src/client/src/candidaturePlugin/components/submit.vue
@@ -17,23 +17,23 @@
       </svg>
     </button>
     <!--
-          <input class="previousButton" type="submit" value="RETOUR" />
-          <button>
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9.70711 17.2929C10.0976 17.6834 10.0976 18.3166 9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071L2.29289 12.7071C1.90237 12.3166 1.90237 11.6834 2.29289 11.2929L8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289C10.0976 5.68342 10.0976 6.31658 9.70711 6.70711L5.41421 11L21 11C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13L5.41422 13L9.70711 17.2929Z"
-              fill="black"
-            />
-          </svg>
-        </button>
-        <input class="submitButton2" type="submit" value="SUIVANT" />
-      --><button class="mainSubmitButton" type="submit">
+            <input class="previousButton" type="submit" value="RETOUR" />
+            <button>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.70711 17.2929C10.0976 17.6834 10.0976 18.3166 9.70711 18.7071C9.31658 19.0976 8.68342 19.0976 8.29289 18.7071L2.29289 12.7071C1.90237 12.3166 1.90237 11.6834 2.29289 11.2929L8.29289 5.29289C8.68342 4.90237 9.31658 4.90237 9.70711 5.29289C10.0976 5.68342 10.0976 6.31658 9.70711 6.70711L5.41421 11L21 11C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13L5.41422 13L9.70711 17.2929Z"
+                fill="black"
+              />
+            </svg>
+          </button>
+          <input class="submitButton2" type="submit" value="SUIVANT" />
+        --><button class="mainSubmitButton" type="submit">
       <div class="mainSubmitButtonTxt">SUIVANT</div>
       <div class="mainSubmitButtonSvg">
         <svg
@@ -66,3 +66,23 @@ export default {
   },
 };
 </script>
+<style src="./form-component.scss" lang="scss"></style>
+<style scoped>
+@media screen and (min-width: 400px) and (max-width: 450px) {
+  .mainSubmitButtonTxt {
+    margin-left: 2.15rem;
+  }
+}
+@media screen and (max-width: 350px) {
+  .mainSubmitButton {
+    margin-left: 0.5rem;
+  }
+  .mainSubmitButtonTxt {
+    margin-left: 1rem;
+  }
+  .mainSubmitButtonSvg {
+    width: auto;
+    max-width: 3rem;
+  }
+}
+</style>
