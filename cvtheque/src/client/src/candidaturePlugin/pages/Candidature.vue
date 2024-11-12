58ea3651e4c7db210bc8646eb250887a076edc52
Patrick Wu <patrickwugerator@gmail.com>
2024-11-12 11:09:14 +0100

    stylisation page accueil

--- cvtheque/src/client/src/candidaturePlugin/pages/Candidature.vue
+++ cvtheque/src/client/src/candidaturePlugin/pages/Candidature.vue
@@ -196,6 +196,18 @@ export default {
 
 <style src="./view-application.scss" lang="scss"></style>
 <style scoped>
+* {
+  font-family: 'DM Sans', sans-serif;
+}
+
+h1{
+  font-size: 4rem;
+  
+}
+p{
+  font-size: 1.3rem;
+  margin-bottom: 1rem;
+}
 .title-container,
 #olecio-logo {
   display: none;
@@ -219,6 +231,9 @@ export default {
   flex-direction: row;
   height: 100vh;
   background-color: #1b1f23;
+}
+
+#accueil h1, #accueil a{
   color: white;
 }
 
@@ -254,7 +269,7 @@ export default {
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  margin: 3rem 4rem;
+  margin: 0rem 4rem;
   gap: 1rem;
   height: 100%;
 }
@@ -283,7 +298,10 @@ export default {
   display: flex;
   flex-direction: row;
   align-self: end;
-  padding-right: 4rem;
+  padding-right: 5rem;
+  height: 4rem;
+  align-items: center;
+  font-weight: bold;
 }
 
 .accueilNav a {
@@ -297,7 +315,6 @@ export default {
   background-color: white;
   border-radius: 10%;
   margin-right: 4rem;
-  margin-top: 1rem;
   height: 70%;
 }
 
@@ -311,10 +328,10 @@ export default {
 
 .commentGauche,
 .aProposDroite {
-  width: 50%;
+  box-sizing: border-box;
+  width: 55%;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
   padding: 4rem;
   justify-content: center;
 }
@@ -324,6 +341,7 @@ export default {
   display: flex;
   flex-direction: row;
   gap: 1rem;
+  margin-top: 1rem;
 }
 
 .commentBouton button,
@@ -346,8 +364,9 @@ export default {
 
 #comment img,
 #aPropos img {
-  margin: 8rem;
-  width: 30%;
+  margin: auto;
+  width: auto;
+  height: 80%;
 }
 
 #aPropos {
