7ae3787f50f274d64be77ba26489968f57e3942f
tristan <tristanbidault@gmail.com>
2025-02-10 11:52:01 +0100

    add google tag manager script

--- cvtheque/src/vendor/composer/installed.php
+++ cvtheque/src/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'orangehrm/orangehrm',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '67a546976f655cad63892ca5cca28199145cd5ea',
+        'reference' => '52c946cbbe35779b21afd8979e344900a524cb72',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -328,7 +328,7 @@
         'orangehrm/orangehrm' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '67a546976f655cad63892ca5cca28199145cd5ea',
+            'reference' => '52c946cbbe35779b21afd8979e344900a524cb72',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
