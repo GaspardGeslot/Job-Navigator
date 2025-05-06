8f3a7ed43d1b695ca47141fb65f49a8fc2be3b50
GaspardGeslot <geslotgaspard@gmail.com>
2025-05-06 17:12:30 +0100

    Add temp work

--- cvtheque/src/client/themes/olecio/orangehrmAdminPlugin/components/MatchingCard.vue
+++ cvtheque/src/client/themes/olecio/orangehrmAdminPlugin/components/MatchingCard.vue
@@ -12,25 +12,26 @@
             :disabled="!editable"
           />
         </oxd-grid-item>
-        <oxd-grid-item class="orangerhrm-switch-wrapper">
-          <oxd-text class="orangehrm-text" tag="p">
-            {{ $t('general.active') }}
-          </oxd-text>
-          <oxd-switch-input v-model="matching.isActive" :disabled="!editable" />
+        <oxd-grid-item>
+          <oxd-input-field
+            v-model="matching.price"
+            :label="$t('Prix')"
+            :disabled="!editable"
+          />
         </oxd-grid-item>
       </oxd-grid>
       <oxd-grid :cols="3" class="orangehrm-full-width-grid">
         <oxd-grid-item>
           <oxd-input-field
-            v-model="matching.maxAmount"
-            :label="$t('Quantité maximale')"
+            v-model="matching.maxAmountPerDay"
+            :label="$t('Quantité maximale par jour')"
             :disabled="!editable"
           />
         </oxd-grid-item>
         <oxd-grid-item>
           <oxd-input-field
-            v-model="matching.price"
-            :label="$t('Prix')"
+            v-model="matching.maxAmountPerMonth"
+            :label="$t('Quantité maximale par mois')"
             :disabled="!editable"
           />
         </oxd-grid-item>
@@ -49,6 +50,14 @@
             :label="$t('Date de fin')"
             :disabled="!editable"
           />
+        </oxd-grid-item>
+      </oxd-grid>
+      <oxd-grid :cols="4" class="orangehrm-full-width-grid">
+        <oxd-grid-item class="orangerhrm-switch-wrapper">
+          <oxd-text class="orangehrm-text" tag="p">
+            {{ $t('general.active') }}
+          </oxd-text>
+          <oxd-switch-input v-model="matching.isActive" :disabled="!editable" />
         </oxd-grid-item>
       </oxd-grid>
       <oxd-divider />
@@ -105,9 +114,54 @@
           />
         </oxd-grid-item>
       </oxd-grid>
-      <br />
       <oxd-divider />
       <br />
+      <oxd-grid :cols="4" class="orangehrm-full-width-grid">
+        <oxd-grid-item class="orangerhrm-switch-wrapper">
+          <oxd-text class="orangehrm-text" tag="p">
+            {{ $t('CV obligatoire') }}
+          </oxd-text>
+          <oxd-switch-input
+            v-model="matching.isResumeNeeded"
+            :disabled="!editable"
+          />
+        </oxd-grid-item>
+      </oxd-grid>
+      <br />
+      <oxd-divider />
+      <oxd-text class="orangehrm-sub-title" tag="h6">
+        {{ $t('Métiers') }}
+      </oxd-text>
+      <oxd-grid :cols="2" class="orangehrm-full-width-grid">
+        <oxd-grid-item class="orangehrm-job-selection-criteria --span-column-2">
+          <oxd-input-field
+            v-model="selectedJobs"
+            type="autocomplete"
+            :label="$t('Métier')"
+            :clear="true"
+            :create-options="loadJobs"
+            :placeholder="$t('Rechercher un métier')"
+            :multiple="true"
+          />
+          <oxd-input-group>
+            <oxd-icon-button
+              style="margin-left: 1rem"
+              name="plus"
+              @click="addJob"
+            />
+          </oxd-input-group>
+        </oxd-grid-item>
+        <oxd-grid-item
+          v-for="(job, index) in matching.jobs"
+          :key="index"
+          class="orangehrm-job-selection-criteria-selected --offset-column-1"
+        >
+          <oxd-icon-button name="trash-fill" @click="onClickDeleteJob" />
+          <oxd-text class="orangehrm-job-selection-criteria-name">
+            {{ job }}
+          </oxd-text>
+        </oxd-grid-item>
+      </oxd-grid>
       <oxd-divider />
       <oxd-text class="orangehrm-sub-title" tag="h6">
         {{ $t('Pays') }}
@@ -118,7 +172,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedCountries"
+            v-model="matching.countries"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -136,7 +190,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedCourseStarts"
+            v-model="matching.courseStarts"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -154,7 +208,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedFundings"
+            v-model="matching.fundings"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -172,7 +226,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedHandicaps"
+            v-model="matching.handicaps"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -190,7 +244,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedLevels"
+            v-model="matching.levels"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -208,7 +262,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedNeeds"
+            v-model="matching.needs"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -226,7 +280,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedPhones"
+            v-model="matching.phones"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -244,7 +298,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedStatus"
+            v-model="matching.status"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -262,7 +316,7 @@
           :key="`${elemIndex}-${elem}`"
         >
           <oxd-input-field
-            v-model="matching.checkedTrainingMethods"
+            v-model="matching.trainingMethods"
             type="checkbox"
             :label="elem.label"
             :value="elem.label"
@@ -287,33 +341,58 @@
 </template>
 <script>
 import {OxdSwitchInput} from '@ohrm/oxd';
+import {APIService} from '@/core/util/services/api.service';
 
 const MatchingModel = {
   id: null,
   isActive: true,
   actor: null,
-  ages: [],
-  checkedCountries: [],
-  checkedCourseStarts: [],
+  isResumeNeeded: false,
+  ages: {
+    type: Object,
+    default: () => ({
+      min: null,
+      max: null,
+    }),
+  },
+  countries: [],
+  courseStarts: [],
   courses: [],
   departments: [],
-  checkedNeeds: [],
-  endBreakDate: null,
-  startBreakDate: null,
+  needs: [],
+  endBreakDate: {
+    type: Object,
+    default: () => ({
+      dayOfWeek: null,
+      hour: null,
+      minutes: null,
+    }),
+  },
+  startBreakDate: {
+    type: Object,
+    default: () => ({
+      dayOfWeek: null,
+      hour: null,
+      minutes: null,
+    }),
+  },
   endDate: null,
   startDate: null,
-  checkedFundings: [],
-  checkedHandicaps: [],
+  fundings: [],
+  handicaps: [],
   title: null,
   jobs: [],
-  checkedLevels: [],
+  levels: [],
   locationPostalCodes: [],
-  maxAmount: 0,
-  checkedPhones: [],
+  maxAmountPerDay: 0,
+  maxAmountPerMonth: 0,
+  phones: [],
   price: 0.0,
   resumeNeeded: false,
-  checkedStatus: [],
-  checkedTrainingMethods: [],
+  status: [],
+  trainingMethods: [],
+  professionalExperiences: [],
+  drivingLicenses: [],
 };
 
 export default {
@@ -327,6 +406,10 @@ export default {
     matchingCurrent: {
       type: Object,
       required: true,
+    },
+    actors: {
+      type: Array,
+      default: () => [],
     },
     countries: {
       type: Array,
@@ -364,12 +447,30 @@ export default {
       type: Array,
       default: () => [],
     },
+    professionalExperiences: {
+      type: Array,
+      default: () => [],
+    },
+    drivingLicenses: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup() {
+    const http = new APIService(
+      window.appGlobal.baseUrl,
+      `${window.appGlobal.theme}/api/v2/admin/job/search`,
+    );
+    return {
+      http,
+    };
   },
   data() {
     return {
       isLoading: false,
-      editable: false,
+      editable: true,
       matching: {...MatchingModel},
+      selectedJobs: [],
     };
   },
   watch: {
@@ -381,12 +482,32 @@ export default {
     this.fetchMatching();
   },
   methods: {
+    addJob() {
+      for (const job of this.selectedJobs) this.matching.jobs.push(job.label);
+      this.selectedJobs = [];
+    },
+    loadJobs(searchParam) {
+      return new Promise((resolve) => {
+        if (searchParam.trim() && searchParam.length < 100) {
+          this.http
+            .getAll({
+              title: searchParam.trim(),
+            })
+            .then(({data}) => {
+              resolve(data);
+            });
+        } else {
+          resolve([]);
+        }
+      });
+    },
     fetchMatching() {
       this.isLoading = true;
       this.matching.id = this.matchingCurrent.id;
       this.matching.title = this.matchingCurrent.title;
       this.matching.isActive = this.matchingCurrent.isActive;
-      this.matching.maxAmount = this.matchingCurrent.maxAmount;
+      this.matching.maxAmountPerDay = this.matchingCurrent.maxAmountPerDay;
+      this.matching.maxAmountPerMonth = this.matchingCurrent.maxAmountPerMonth;
       this.matching.price = this.matchingCurrent.price;
       this.matching.startDate = this.matchingCurrent.startDate;
       this.matching.endDate = this.matchingCurrent.endDate;
@@ -398,23 +519,46 @@ export default {
         this.matchingCurrent.endBreakDate !== null
           ? this.matchingCurrent.endBreakDate
           : {dayOfWeek: '', hour: '', minutes: ''};
-      this.matching.checkedCountries = this.matchingCurrent.checkedCountries;
-      this.matching.checkedCourseStarts =
-        this.matchingCurrent.checkedCourseStarts;
-      this.matching.checkedFundings = this.matchingCurrent.checkedFundings;
-      this.matching.checkedHandicaps = this.matchingCurrent.checkedHandicaps;
-      this.matching.checkedLevels = this.matchingCurrent.checkedLevels;
-      this.matching.checkedNeeds = this.matchingCurrent.checkedNeeds;
-      this.matching.checkedPhones = this.matchingCurrent.checkedPhones;
-      this.matching.checkedStatus = this.matchingCurrent.checkedStatus;
-      this.matching.checkedTrainingMethods =
-        this.matchingCurrent.checkedTrainingMethods;
+      this.matching.countries = this.matchingCurrent.countries;
+      this.matching.courseStarts = this.matchingCurrent.courseStarts;
+      this.matching.fundings = this.matchingCurrent.fundings;
+      this.matching.handicaps = this.matchingCurrent.handicaps;
+      this.matching.levels = this.matchingCurrent.levels;
+      this.matching.needs = this.matchingCurrent.needs;
+      this.matching.phones = this.matchingCurrent.phones;
+      this.matching.status = this.matchingCurrent.status;
+      this.matching.trainingMethods = this.matchingCurrent.trainingMethods;
+      this.matching.professionalExperiences =
+        this.matchingCurrent.professionalExperiences;
+      this.matching.drivingLicenses = this.matchingCurrent.drivingLicenses;
+      this.matching.isResumeNeeded = this.matchingCurrent.isResumeNeeded;
+      this.matching.jobs = ['Test'];
       this.isLoading = false;
     },
   },
 };
 </script>
 <style scoped lang="scss">
+.orangehrm-job-selection {
+  &-criteria {
+    display: flex;
+    align-items: center;
+  }
+  &-criteria-selected {
+    display: flex;
+    align-items: baseline;
+  }
+  &-criteria-name {
+    margin-left: 1rem;
+    font-weight: 700;
+    font-size: $oxd-input-control-font-size;
+    padding: $oxd-input-control-vertical-padding 0rem;
+  }
+  &-icon {
+    margin-left: 1rem;
+  }
+}
+
 .orangerhrm-switch-wrapper {
   display: flex;
   flex-direction: row;
