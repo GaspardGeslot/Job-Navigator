3e6f90e5263105421ba046afcd456a0bd0f5cf6d
GaspardGeslot <geslotgaspard@gmail.com>
2026-01-16 13:19:43 +0000

    Add users management for maraudes actor

--- cvtheque/src/client/themes/maraudes/pages.ts
+++ cvtheque/src/client/themes/maraudes/pages.ts
@@ -2,10 +2,12 @@ import CorePages from '@/core/pages';
 import HomePages from './home';
 import AuthenticationPages from './authentication';
 import RecruitmentPages from './recruitment';
+import AdminPages from './admin';
 
 export default {
   ...AuthenticationPages,
   ...HomePages,
   ...CorePages,
   ...RecruitmentPages,
+  ...AdminPages,
 };
